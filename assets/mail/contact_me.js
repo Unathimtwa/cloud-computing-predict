5c3d3a0b499c27f7dff5fd4683ed5f21a27248c4
Unathi <Mtwaunathi@gmail.com>
2023-10-17 21:52:32 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://w63b6bzh6i.execute-api.eu-west-1.amazonaws.com/default/UnathiM_Lamndafunc",
+                url: "https://u32bctf1na.execute-api.eu-west-1.amazonaws.com/UnathiM_Func",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
